1d23f14a0a6f35634851a692ea81cf7b2cfea2b7
Amacuse <mercc@rambler.ru>
2016-12-11 19:16:26 +0200

    Add the notification for a failure case

--- public/javascripts/forDelete.js
+++ public/javascripts/forDelete.js
@@ -1,9 +1,11 @@
 function del(id) {
     var result = confirm("Confirm the deletion");
     if (result) {
-        $.ajax(jsRoutes.controllers.FieldController.deleteField(id))
-            .done(function (results) {
-                location.reload();
-            }).fail(/**/);
+        $.ajax(jsRoutes.controllers.FieldController.deleteField(id)).
+        done(function () {
+            location.reload();
+        }).fail(
+            alert("Cannot delete now.Please try later")
+        );
     }
 }
